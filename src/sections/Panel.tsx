ad38d5c57780de4de662f1522f3b0380c63da512
wjlfish <wjl@stu.scu.edu.cn>
2025-03-19 19:41:31 +0800

    Update Panel.tsx

--- src/sections/Panel.tsx
+++ src/sections/Panel.tsx
@@ -10,6 +10,7 @@ import OrderManage from "../tabs/OrderManage";
 import EventManage from "../tabs/EventManage";
 import Statistics from "../tabs/Statistics";
 import Settings from "../tabs/Settings";
+import Lucky from "../sections/lucky/Lucky";
 import { NoticeType } from "antd/es/message/interface";
 
 type Props = {
@@ -27,37 +28,17 @@ type State = {
 export default class Panel extends Cp<Props, State>{
     items :ItemType<MenuItemType>[] = [
         {
-            key: "users",
-            label: "用户管理",
+            key: "lucky",
+            label: "抽取管理",
             icon: <IdcardOutlined />
-        },
-        {
-            key: "orders",
-            label: "工单管理",
-            icon: <SnippetsOutlined />
-        },
-        {
-            key: "events",
-            label: "活动管理",
-            icon: <FlagOutlined />
-        },
-        {
-            key: "stats",
-            label: "统计数据",
-            icon: <BarChartOutlined />
-        },
-        {
-            key: "settings",
-            label: "设置",
-            icon: <SettingOutlined />
         }
     ];
-    mapping = ["users", "orders", "events", "stats", "settings"];
+    mapping = ["users", "lucky", "orders", "events", "stats", "settings"];
     constructor(props :Props){
         super(props);
         const hash = window.location.hash.substring(1, window.location.hash.length), index = this.mapping.indexOf(hash);
         this.state = {
-            currentKey: index !== -1 ? hash : "users",
+            currentKey: index !== -1 ? hash : "lucky",
             aboutModalOpened: false,
             menuRefresher: Date.now()
         };
@@ -102,8 +83,8 @@ export default class Panel extends Cp<Props, State>{
                     alignItems: "center",
                     gap: "1rem"
                 }}>
-                    <div>{meta.version} {meta.dev ? "开发版" : ""}</div>
-                    <div>
+                    {/* <div>{meta.version} {meta.dev ? "开发版" : ""}</div> */}
+                    {/* <div>
                         <ConfigProvider theme={{components: {Button: {paddingInline: 0, textHoverBg: "transparent", colorBgTextActive: "transparent"}}}}><Button type="text" onClick={()=>this.setState({aboutModalOpened: true})}>关于</Button></ConfigProvider>
                         <Modal
                             open={this.state.aboutModalOpened}
@@ -112,13 +93,9 @@ export default class Panel extends Cp<Props, State>{
                             width={"60dvw"}
                             closable={false}
                         >{meta.about}</Modal>
-                    </div>
+                    </div> */}
                 </div>
-                {this.state.currentKey === "users" ? <UserManage sendMessage={this.props.sendMessage} ATFailCallBack={this.props.ATFailCallBack} /> : null}
-                {this.state.currentKey === "orders" ? <OrderManage ATFailCallBack={this.props.ATFailCallBack} /> : null}
-                {this.state.currentKey === "events" ? <EventManage ATFailCallBack={this.props.ATFailCallBack} /> : null}
-                {this.state.currentKey === "stats" ? <Statistics ATFailCallBack={this.props.ATFailCallBack} /> : null}
-                {this.state.currentKey === "settings" ? <Settings fromLucky={false} ATFailCallBack={this.props.ATFailCallBack} /> : null}
+                {this.state.currentKey === "lucky" ? <Lucky ATFailCallBack={this.props.ATFailCallBack} /> : null}
             </div>
         );
     }
